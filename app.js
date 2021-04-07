f4470182509e53f1523f5bf456aad2ba53be1df3
Abror777 <wiuterian2019@gmail.com>
2021-04-07 20:50:44 +0500

    Error fixes and summing up

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.get('/create', (req, res) => {
 app.post('/create', (req, res) => {
 	const name = req.body.name
 	const surname = req.body.surname
+	const email= req.body.email
+	const phone = req.body.phone
 	const description = req.body.description
 
-	if (name.trim() === '' && surname.trim() === '' && description.trim() === '' ) {
+	if (name.trim() === '' && surname.trim() === '' && email.trim() === '' && phone.trim() === '' && description.trim() === '' ) {
 		res.render('create', { error: true})
 	} else {
 		fs.readFile('./data/applications.json', (err, data) => {
@@ -36,6 +38,8 @@ app.post('/create', (req, res) => {
 				name: name,
 				surname: surname,
 				description: description, 
+				email: email,
+				phone: phone,
 			})
 
 			fs.writeFile('./data/applications.json', JSON.stringify(applications), err => {
@@ -47,6 +51,16 @@ app.post('/create', (req, res) => {
 	}
 
 })
+
+app.get('/api/v1/applications', (req, res) => {
+	fs.readFile('./data/applications.json', (err, data) => {
+	  if (err) throw err
+  
+	  const applications = JSON.parse(data)
+  
+	  res.json(applications)
+	})
+  })
 
 app.get('/applications', (req, res) => {
 	fs.readFile('./data/applications.json', (err, data) => {
@@ -72,12 +86,75 @@ app.get('/applications/:id', (req, res) => {
 	})
 })
 
+app.get('/:id/delete', (req, res) => {
+	const id = req.params.id
+
+	fs.readFile('./data/applications.json', (err, data) => {
+		if  (err) throw err
+
+		const applications = JSON.parse(data)
+
+		const filteredapplications = applications.filter(application => application.id != id)
+
+		fs.writeFile('./data/applications.json', JSON.stringify(filteredapplications), (err) => {
+			if (err) throw err
+
+			res.render('applications', {applications, deleted: true})
+		})
+	})
+})
+
+app.get('/applications/:id/archive', (req, res) => {
+	const id = req.params.id
+
+	fs.readFile('./data/applications.json', (err, data) => {
+	  if (err) throw err
+	  
+	  const applications = JSON.parse(data)
+	  const applicationId  = applications.filter(application => application.id === id)
+	  const splicedId = applications.splice(applicationId,1)[0]
+	  applications.push(splicedId)
+
+	  fs.writeFile('./data/application.json', JSON.stringify(applications), (err) => {
+		if (err) throw err
+
+		res.redirect('/applications')
+
+	})
+})
+})
+
+
+  app.get('/archive',(req,res)=>{
+	fs.readFile('./data/applications.json', (err, data) => {
+		if (err) throw err
+		
+
+		const applications = JSON.parse(data).filter(application => application.archive == true)
+		res.render("applications", { applications: applications });
+
+  })
+})
+
+  var multer  = require('multer')
+  var storage = multer.diskStorage({
+	  destination: function (req, file, cb) {
+		  cb(null, '/static/images/')
+	  },
+	  filename: function (req, file, cb) {
+		  cb(null, file.originalname)
+	}
+  })
+  
+  var upload = multer({ storage: storage })
+  
+
 app.listen(8000, err => {
 	if (err) console.log(err)
 
 	console.log('Server is running: Port-8000')
 })
 
-function id () {
+function id() {
 	return '_' + Math.random().toString(36).substr(2, 9);
   }
